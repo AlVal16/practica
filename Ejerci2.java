df708ef6f83f6ae70f58f457cbf03abd96dcd60a
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-05-20 15:26:08 -0500

    Update Ejerci2.java

--- Ejerci2.java
+++ Ejerci2.java
@@ -4,13 +4,13 @@ public class Ejerci2 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int num, n=1;
-		System.out.println("Ingresa un número");
+		int num;
+		System.out.println("Ingresa un nÃºmero");
 		num = scan.nextInt();
 
 		while (num >= 0) {
 			n+=1;
-			System.out.println("Siguiente número");
+			System.out.println("Siguiente nÃºmero");
 			num = scan.nextInt();
 		}
 		System.out.println(" Ingresaste = "+n);
