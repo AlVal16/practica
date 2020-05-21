7139d8c1d30eeb823090d684e3a85ba48495b35c
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-05-21 14:33:22 -0500

    Add files via upload

---
+++ Reto1.java
@@ -0,0 +1,19 @@
+
+public class Reto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num;
+		num=1;
+		
+		while(num<10) {
+		num+=2;
+		if (num!=7) {
+				System.out.println(num);
+			}
+		}
+		
+	}
+
+}
