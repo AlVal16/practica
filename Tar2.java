d344ae597a24fe3585ecc7f3d817076eec8d69c9
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-05-21 17:37:51 -0500

    Add files via upload

---
+++ Tar2.java
@@ -0,0 +1,19 @@
+
+public class Tar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num, sumanu = 0;
+		num = 1;
+
+		while (num < 40) {
+			;
+			num += 3;
+			System.out.println(num);
+			sumanu = sumanu + num;
+		}
+		System.out.println(" La suma es: " + sumanu);
+
+	}
+}
