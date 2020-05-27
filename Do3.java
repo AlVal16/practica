7d6169e89564698177acd95e68e8beb5771c3325
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-05-27 15:00:49 -0500

    Add files via upload

---
+++ Do3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Do3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int num,suma,cont,i;
+	    suma=0;
+	    cont=1;
+	    i=0;
+	    
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese cantidad de números ");
+			
+		do {
+			num=scan.nextInt();
+		
+		cont++;
+		suma=cont+num;
+		System.out.println("Ingrese número"+cont);
+		}
+		while( num>i);
+		System.out.println("La suma es "+suma);
+	}
+
+}
