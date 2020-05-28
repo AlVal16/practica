607377d14478bee5660466bfc0ea037e70a81d72
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-05-28 14:07:35 -0500

    Add files via upload

---
+++ Do5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Do5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int edad, n, suma,i;
+		double prom;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("ingresa cantidad de alumnos");
+		n= scan.nextInt();
+		suma = 0;
+		i=1;
+		do {
+			System.out.println("ingrese edad");
+			edad = scan.nextInt();
+			suma = suma + edad;
+			i++;
+		} while (i<= n);
+		prom=suma/n;
+		System.out.println(prom);
+
+	}
+}
+	
