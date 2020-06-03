d38b7044edfe63c2782078699a9da69064715dfd
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-06-03 14:34:24 -0500

    Add files via upload

---
+++ ForReto1.java
@@ -0,0 +1,25 @@
+
+public class ForReto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=3,n2=4,n3=5,r1,r2,r3,i;
+		for  (i=1; i<=12;i++) {
+			r1=n1*i;
+			System.out.println(n1+" x "+i+" = "+r1);
+			}	
+		System.out.println();
+		for  (i=1; i<=12;i++) {
+			r2=n2*i;
+			System.out.println(n2+" x "+i+" = "+r2);
+			}	
+		System.out.println();
+		for  (i=1; i<=12;i++) {
+			r3=n3*i;
+			System.out.println(n3+" x "+i+" = "+r3);
+			}	
+		
+		
+	}
+
+}
