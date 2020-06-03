03ee1cff290d6b39e27b50ce23d0569ec569824c
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-06-03 15:01:09 -0500

    Update Foret2.java

--- Foret2.java
+++ Foret2.java
@@ -4,19 +4,22 @@ public class Foret2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//pedir un nmero N, introducir N sueldos y mostrar el sueldo máximo
-		int N, NS = 0, SM = 0, i, j;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Ingresar un número");
-		N = scan.nextInt();
-		for (i = 1; i <= N; i++) {
-				System.out.println("Ingresar sueldo");
-				NS=scan.nextInt();
-				SM = SM + NS;
-			
-		}
-		System.out.println("El sueldo máximo es " + SM);
-
+//pedir un nmero N, introducir N sueldos y mostrar el sueldo mÃ¡ximo
+		 Scanner scan = new Scanner(System.in);
+	        int n, sueldo, i, sueldomax;
+	        sueldomax = 0;
+	        
+	        System.out.println("Ingrese un numero");
+	        n = scan.nextInt();
+	        
+	        for(i = 1; i<=n; i++){
+	            System.out.println("Ingresa el sueldo");
+	            sueldo = scan.nextInt();
+	            if(sueldo>=sueldomax){
+	                sueldomax = sueldo;
+	            }
+	        }
+	        System.out.println("El sueldo maximo es: " + sueldomax);
+	    }
 	}
 
-}
