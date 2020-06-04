4e0ba54dd86865ae74dce0ecb1587109c64e8798
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-06-04 14:53:06 -0500

    Update Forr2.java

--- Forr2.java
+++ Forr2.java
@@ -4,14 +4,14 @@ public class Forr2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n, sn = 0, i = 1;
+		int n, sn = 0, i = 0;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Ingresar número");
+		System.out.println("Ingresar nÃºmero");
 
 		for (n = scan.nextInt(); n > 0; i++) {
 			System.out.println("Numero: ");
 			n = scan.nextInt();
 		}
-		System.out.println("Se han introducido : "+i+ "números");
+		System.out.println("Se han introducido : "+i+ "nÃºmeros");
 	}
 }
