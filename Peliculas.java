b1928de68c51d08ee3264ac643d6d63682304d2f
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-06-24 14:50:36 -0500

    Add files via upload

---
+++ Peliculas.java
@@ -0,0 +1,20 @@
+
+public class Peliculas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		accion myaccion=new accion();
+		myaccion.aaccion();
+		
+		Anime myanime=new Anime();
+		myanime.aanime();
+		
+		Terror myterror=new Terror();
+		myterror.tterror();
+		
+		Suspenso mysuspenso=new Suspenso();
+		mysuspenso.ssupenso();
+	}
+
+}
