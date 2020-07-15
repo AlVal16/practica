7ab7f36bb30b8f34fa69b3ccea56e3c953824774
AlVal16 <63749741+AlVal16@users.noreply.github.com>
2020-07-15 15:34:11 -0500

    Add files via upload

---
+++ AnimalHerbivoro.java
@@ -0,0 +1,10 @@
+
+public class AnimalHerbivoro extends Animal{
+	public void alimentarse() {
+		System.out.println("Los animales se alimentan de hierba");
+	}
+	public AnimalHerbivoro() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Planta.java
@@ -0,0 +1,11 @@
+
+public class Planta extends Servivo {
+
+	public void alimentarse() {
+		System.out.println("Las plantas se alimentan de agua y sol");
+	}
+	public Planta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
